399125ff3fc262119918e18fb3660267361b555d
Adeola Idowu <adeolaidowu27@gmail.com>
2024-02-06 16:01:37 +0100

    implement search and sort functionality on products

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,19 @@
 import ProductList from '@/components/ProductList';
 
+async function getProducts() {
+  const res = await fetch('https://dummyjson.com/products')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+
 export default async function Home() {
+  const { products } = await getProducts()
   return <main>
     <h2>Product List</h2>
-    <ProductList />
+    <ProductList products={products} />
   </main>
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,28 +1,44 @@
+"use client"
+import { useState } from 'react'
 import Image from 'next/image'
 
-async function getProducts() {
-    const res = await fetch('https://fakestoreapi.com/products')
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
 
-    return res.json()
-}
+export default function ProductList({ products }) {
 
-export default async function ProductList() {
-    const products = await getProducts()
+    const [searchValue, setSearchValue] = useState('');
+    const [sortOrder, setSortOrder] = useState('asc');
+
+    const filteredProducts = products.filter(product => product.title.toLowerCase().includes(searchValue.toLowerCase()));
+
+    const sortedProducts = [...filteredProducts].sort((a, b) => {
+        return sortOrder === 'asc' ? a.price - b.price : b.price - a.price;
+    });
+
     return (
         <div className="product-list">
-            {products.map(product => (
+
+            <input
+                type="text"
+                placeholder="Search products..."
+                value={searchValue}
+                onChange={e => setSearchValue(e.target.value)}
+            />
+
+            <select value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
+                <option value="asc">Price: Low to High</option>
+                <option value="desc">Price: High to Low</option>
+            </select>
+
+            {sortedProducts.map(product => (
                 <div key={product.id} className="product">
                     <div className="product-img">
-                        <Image src={product.image} width={500}
-                            height={500}
+                        <Image src={product.images[0]} width={200}
+                            height={200}
                             alt="Picture of the product" />
                     </div>
                     <div className="product-details">
                         <h3>{product.title}</h3>
-                        <p>Price: N{product.price}</p>
+                        <p>Price: #{product.price}</p>
                     </div>
                     <button>Add to Cart</button>
                 </div>
